a7a8bceb071c0d7c981729ed0a856b2e8af10b24
Prajakta-Shelar <prajaktashelar109@gmail.com>
2024-09-05 13:21:28 +0530

    done all changes

--- environment.ts
+++ environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://meetmyartist.beatsacademy.in',
+    apiUrl: 'https://api.meetmyartist.in',
 };
--- src/app/shared/service/auth.service.ts
+++ src/app/shared/service/auth.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-// import { environment } from 'environment';
+import { environment } from 'environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  // private url = environment.apiUrl;
+  private url = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
   login(aname: string, apassword: string): Observable<any> {
     const data = { aname, apassword };
-    return this.http.post<any>(`https://meetmyartist.beatsacademy.in/adminLogin/`, data);
+    return this.http.post<any>(`${this.url}/adminLogin/`, data);
   }
 
   isLoggedIn() {
